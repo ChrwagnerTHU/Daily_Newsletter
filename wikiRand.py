d3d5ab5e1c608cad04b9c36081ffcd5e6756f0f5
ChrwagnerTHU <wagnch02@thu.de>
2022-09-17 17:47:29 +0200

    Update random Wiki article and Mail Layout

--- wikiRand.py
+++ wikiRand.py
@@ -5,8 +5,9 @@ def main():
     url = requests.get("https://de.wikipedia.org/wiki/Special:Random")
     soup = BeautifulSoup(url.content, "html.parser")
     title = soup.find(class_="firstHeading").text
+    titleURL = title.replace(" ", "_")
 
-    return {"Link": "https://de.wikipedia.org/wiki/%s" % title, "Header": title}
+    return {"Link": "https://de.wikipedia.org/wiki/%s" % titleURL, "Header": title}
 
 if __name__ == '__main__':
     main()
