757da9596044afc64d957a8515029daa833a59ea
Jonas Steinhauser <info@jonas-steinhauser.de>
2022-11-14 17:22:23 +0100

    new testscript to start data request

--- finance_requests/test.py
+++ finance_requests/test.py
@@ -1,10 +1,12 @@
-import json
 import finance_request
-
-symbols = {"AIR"}
+ 
 requester = finance_request.requester("J1AF3SZ0S4184JGT", 'stock_info.json')
 
+# for test usage (2nd time, after data is saved as data.json)
+#requester = finance_request.requester()
+
+# get data and save it as data.json
 data = requester.getDataDaily()
 
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+# plot data and save as png with userid
+requester.plot("steinie")
